028de0d2b9eae29f7588a66c829440de0f8de966
joaopaulinho1212 <66037072+joaopaulinho1212@users.noreply.github.com>
2020-06-05 15:26:32 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,5 +5,5 @@ require "cpf_cnpj"
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = "https://www.netshoes.com.br"
+    config.app_host = "Site de Compras Esportivos"
 end    
